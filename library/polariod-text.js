b0a7c1bb2298b9e48f5033f7599d4dcabf209415
Abhishek Dey <adey@innominds.com>
2017-01-21 22:23:15 +0530

    deploy in heroku

---
+++ library/polariod-text.js
@@ -0,0 +1,27 @@
+/**
+ * Created by adey on 19-01-2017.
+ */
+
+
+/*
+
+ $(function() {
+ $('a.page-scroll').bind('click', function(event) {
+ var $anchor = $(this);
+ $('html, body').stop().animate({
+ scrollTop: $($anchor.attr('href')).offset().top
+ }, 1500, 'easeInOutExpo');
+ event.preventDefault();
+ });
+ });
+ */
+
+// Highlight the top nav as scrolling occurs
+/*
+$(function () {
+    $('.pics a').hover(function () {
+        $(this).parent().next('.gov-cat .tooltip1').removeClass('hidden');
+    }, function () {
+        $(this).parent().next('.gov-cat .tooltip1').addClass('hidden');
+    });
+});*/
